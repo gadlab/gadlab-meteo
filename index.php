64656d707c3c0aa2899e2e2f60846db06ed21b29
Pierre-Yves Gadina <pygadina@gmail.com>
2021-08-30 22:13:21 +0200

    glm v2
    Restarted from scratch

--- index.php
+++ index.php
@@ -1,122 +1,65 @@
 <?php
 /**
-  * @package gadlab-meteo
-  * @version 1.0
-  */
+*  @package gadlab-meteo
+*  @version 2.0
+*/
+
 /*
 Plugin Name: Gad Lab Meteo
 Plugin URI: https://github.com/gadlab/gadlab-meteo
-Description: Permet d'afficher les prévisions météo locale du col du Marchairuz avec un shortcode
-Author: Gad Lab // web.sites.builder
-Version: 1.0
+Description: Display local weather forecast of «The Marchairuz Pass».
+Author: Gad Lab
 Author URI: http://gadlab.net/
+Version: 2.0
+License: GPLv3 or later
+Text Domain: glm
+Domain Path: /languages
 */
+if ( !defined( 'ABSPATH' ) ) die( 'No direct access allowed' );
 
-function meteo_decode( $atts ) {
-
-	/*	PRÉVISION MÉTÉO
-		---------------------------------
-		GUIDE DE RÉCUPÉRATION DES DONNÉES
-		---------------------------------
-		https://www.prevision-meteo.ch/uploads/pdf/recuperation-donnees-meteo.pdf
-	*/
-
-    // Set up default parameters
-    extract(shortcode_atts(array(
-     'type' => 'today'
-    ), $atts));
-
-	// Get json meteo data file and decode it
-	$json = file_get_contents('https://prevision-meteo.ch/services/json/le-marchairuz');
-	$json = json_decode($json);
-
-	// Get the kind of meteo type to display
-	$meteo_type = $atts['type'] ;
-
-	// Set Output Code based on attribute's shortcode
-  if ($meteo_type == 'meteo_today'){
-    // Display today basic widget
-    return '<a href="/informations/meteo/" title="Consulter les prévisions détaillées...">'
-		.'<div id="meteo_today"><p style="color:#ffffff;font-size:2.5em;line-height:1.3em;padding:0;text-align:center;">'
-		.$json->current_condition->tmp.'° <img src="'
-		.$json->current_condition->icon.'" alt="icon"></p><p style="color:#ffffff;text-align:center;"><b>'
-		.$json->current_condition->condition.'</b><br/>Vent : <b>'
-		.$json->current_condition->wnd_dir.'</b> à <b>'
-		.$json->current_condition->wnd_spd.' km/h</b><br/><i class="fas fa-sun" style="color:#fee034;"></i> <i class="fas fa-arrow-up"></i> '
-		.$json->city_info->sunrise.' <i class="fas fa-arrow-down"></i> '
-		.$json->city_info->sunset.'<br/>&nbsp;<i class="fas fa-tint" style="color:#00c3ff;"></i>&nbsp; Humidité : <b>'
-		.$json->current_condition->humidity.'%</b></p></div></a><!-- /#widget_meteo -->';
-	} else if ( $meteo_type == 'meteo_forecast' ){
-    // Display 4 days forecast widget
-		return '<table style="width:100%;color:white;border:0 none;border-collapse:collapse;text-align:center;"><tbody><tr><td id="col1" style="border-top:0 none; padding-top:0!important; padding-bottom:0!important; text-align:center;"><strong>'
-		.$json->fcst_day_0->day_short.'&nbsp;'
-		.$json->fcst_day_0->date.'</strong><br/><img src="'
-		.$json->fcst_day_0->icon_big.'" alt="icon" style="width:100%;"><br/><span style="color:#00c3ff;font-weight:bold;">'
-		.$json->fcst_day_0->tmin.'&deg;</span> / <span style="color:#fe8934;font-weight:bold;">'
-		.$json->fcst_day_0->tmax.'&deg;</span><br/>'
-		.$json->fcst_day_0->condition.'</td><!--/#col1--><td id="col2" style="border-top:0 none; padding-top:0!important; padding-bottom:0!important; text-align:center;"><strong>'
-		.$json->fcst_day_1->day_short.'&nbsp;'
-		.$json->fcst_day_1->date.'</strong><br/><img src="'
-		.$json->fcst_day_1->icon_big.'" alt="icon" style="width:100%;"><br/><span style="color:#00c3ff;font-weight:bold;">'
-		.$json->fcst_day_1->tmin.'&deg;</span> / <span style="color:#fe8934;font-weight:bold;">'
-		.$json->fcst_day_1->tmax.'&deg;</span><br/>'
-		.$json->fcst_day_1->condition.'</td><!--/#col2--><td id="col3" style="border-top:0 none; padding-top:0!important; padding-bottom:0!important; text-align:center;"><strong>'
-		.$json->fcst_day_2->day_short.'&nbsp;'
-		.$json->fcst_day_2->date.'</strong><br/><img src="'
-		.$json->fcst_day_2->icon_big.'" alt="icon" style="width:100%;"><br/><span style="color:#00c3ff;font-weight:bold;">'
-		.$json->fcst_day_2->tmin.'&deg;</span> / <span style="color:#fe8934;font-weight:bold;">'
-		.$json->fcst_day_2->tmax.'&deg;</span><br/>'
-		.$json->fcst_day_2->condition.'</td><!--/#col3--><td id="col4" style="border-top:0 none; padding-top:0!important; padding-bottom:0!important; text-align:center;"><strong>'
-		.$json->fcst_day_3->day_short.'&nbsp;'
-		.$json->fcst_day_3->date.'</strong><br/><img src="'
-		.$json->fcst_day_3->icon_big.'" alt="icon" style="width:100%;"><br/><span style="color:#00c3ff;font-weight:bold;">'
-		.$json->fcst_day_3->tmin.'&deg;</span> / <span style="color:#fe8934;font-weight:bold;">'
-		.$json->fcst_day_3->tmax.'&deg;</span><br/>'
-		.$json->fcst_day_3->condition.'</td><!--/#col4--></tr></tbody></table>';
-	} else if ( $meteo_type == 'meteo_hours' ){
-    //Display hourly forecast
-		return '<style>table.meteo_marchairuz { width: 100%; background-color: rgb(0 0 0 / 25%); border-collapse: collapse; border: 0 none !important; color: #ffffff; } table.meteo_marchairuz td, table.meteo_marchairuz th { border: 1px solid #000000; color: white; font-weight: 400; padding: 5px 0; text-align: center; vertical-align:top; } table.meteo_marchairuz caption {font-weight: bold;}</style>'
-		.'<table class="meteo_marchairuz"><thead><caption>Prévisions horaires du jour ( '
-		.$json->fcst_day_0->day_short.'&nbsp;'
-		.$json->fcst_day_0->date.' )</caption><tr><!--hours--><th>01:00</th><th>03:00</th><th>05:00</th><th>07:00</th><th>09:00</th><th>11:00</th><th>13:00</th><th>15:00</th><th>17:00</th><th>19:00</th><th>21:00</th><th>23:00</th></tr><!--/hours--></thead><tbody><tr><!--icons--><td style="padding-top:72px;"><img src="'
-		.$json->fcst_day_0->hourly_data->{'0H00'}->ICON.'" alt="1h00"><br/><b>'
-		.$json->fcst_day_0->hourly_data->{'0H00'}->TMP2m.'&nbsp;&deg;</b></td><td style="padding-top:60px;"><img src="'
-		.$json->fcst_day_0->hourly_data->{'3H00'}->ICON.'" alt="3h00"><br/><b>'
-		.$json->fcst_day_0->hourly_data->{'3H00'}->TMP2m.'&nbsp;&deg;</b></td><td style="padding-top:48px;"><img src="'
-		.$json->fcst_day_0->hourly_data->{'5H00'}->ICON.'" alt="5h00"><br/><b>'
-		.$json->fcst_day_0->hourly_data->{'5H00'}->TMP2m.'&nbsp;&deg;</b></td><td style="padding-top:36px;"><img src="'
-		.$json->fcst_day_0->hourly_data->{'7H00'}->ICON.'" alt="7h00"><br/><b>'
-		.$json->fcst_day_0->hourly_data->{'7H00'}->TMP2m.'&nbsp;&deg;</b></td><td style="padding-top:24px;"><img src="'
-		.$json->fcst_day_0->hourly_data->{'9H00'}->ICON.'" alt="9h00"><br/><b>'
-		.$json->fcst_day_0->hourly_data->{'9H00'}->TMP2m.'&nbsp;&deg;</b></td><td style="padding-top:12px;"><img src="'
-		.$json->fcst_day_0->hourly_data->{'11H00'}->ICON.'" alt="1100"><br/><b>'
-		.$json->fcst_day_0->hourly_data->{'11H00'}->TMP2m.'&nbsp;&deg;</b></td><td style="padding-top:0px;"><img src="'
-		.$json->fcst_day_0->hourly_data->{'13H00'}->ICON.'" alt="13h00"><br/><b>'
-		.$json->fcst_day_0->hourly_data->{'13H00'}->TMP2m.'&nbsp;&deg;</b></td><td style="padding-top:12px;"><img src="'
-		.$json->fcst_day_0->hourly_data->{'15H00'}->ICON.'" alt="15h00"><br/><b>'
-		.$json->fcst_day_0->hourly_data->{'15H00'}->TMP2m.'&nbsp;&deg;</b></td><td style="padding-top:24px;"><img src="'
-		.$json->fcst_day_0->hourly_data->{'17H00'}->ICON.'" alt="17h00"><br/><b>'
-		.$json->fcst_day_0->hourly_data->{'17H00'}->TMP2m.'&nbsp;&deg;</b></td><td style="padding-top:36px;"><img src="'
-		.$json->fcst_day_0->hourly_data->{'19H00'}->ICON.'" alt="19h00"><br/><b>'
-		.$json->fcst_day_0->hourly_data->{'19H00'}->TMP2m.'&nbsp;&deg;</b></td><td style="padding-top:48px;"><img src="'
-		.$json->fcst_day_0->hourly_data->{'21H00'}->ICON.'" alt="21h00"><br/><b>'
-		.$json->fcst_day_0->hourly_data->{'21H00'}->TMP2m.'&nbsp;&deg;</b></td><td style="padding-top:60px;"><img src="'
-		.$json->fcst_day_0->hourly_data->{'23H00'}->ICON.'" alt="23h00"><br/><b>'
-		.$json->fcst_day_0->hourly_data->{'23H00'}->TMP2m.'&nbsp;&deg;</b></td></tr><!--/icons--><tfoot><!--precipitation--><td style="color:#00c3ff;">'
-		.$json->fcst_day_0->hourly_data->{'1H00'}->APCPsfc.'&nbsp;mm</td><td style="color:#00c3ff;">'
-		.$json->fcst_day_0->hourly_data->{'3H00'}->APCPsfc.'&nbsp;mm</td><td style="color:#00c3ff;">'
-		.$json->fcst_day_0->hourly_data->{'5H00'}->APCPsfc.'&nbsp;mm</td><td style="color:#00c3ff;">'
-		.$json->fcst_day_0->hourly_data->{'7H00'}->APCPsfc.'&nbsp;mm</td><td style="color:#00c3ff;">'
-		.$json->fcst_day_0->hourly_data->{'9H00'}->APCPsfc.'&nbsp;mm</td><td style="color:#00c3ff;">'
-		.$json->fcst_day_0->hourly_data->{'11H00'}->APCPsfc.'&nbsp;mm</td><td style="color:#00c3ff;">'
-		.$json->fcst_day_0->hourly_data->{'13H00'}->APCPsfc.'&nbsp;mm</td><td style="color:#00c3ff;">'
-		.$json->fcst_day_0->hourly_data->{'15H00'}->APCPsfc.'&nbsp;mm</td><td style="color:#00c3ff;">'
-		.$json->fcst_day_0->hourly_data->{'17H00'}->APCPsfc.'&nbsp;mm</td><td style="color:#00c3ff;">'
-		.$json->fcst_day_0->hourly_data->{'19H00'}->APCPsfc.'&nbsp;mm</td><td style="color:#00c3ff;">'
-		.$json->fcst_day_0->hourly_data->{'21H00'}->APCPsfc.'&nbsp;mm</td><td style="color:#00c3ff;">'
-		.$json->fcst_day_0->hourly_data->{'23H00'}->APCPsfc.'&nbsp;mm</td></tfoot><!--/precipitation--></tbody></table>';
-	};
+/* Load plugin textdomain
+----------------------------------------------------------------------------- */
+function glm_load_textdomain() {
+  load_plugin_textdomain( 'glm', false, basename( dirname( __FILE__ ) ) . '/languages/' );
 }
+add_action( 'init', 'glm_load_textdomain' );
 
-// Declare shortcode in Wordpress
-add_shortcode( 'meteo', 'meteo_decode' );
+/* Register and enqueue CSS file into page header
+----------------------------------------------------------------------------- */
+function add_glm_css() {
+  wp_register_style( 'gadlab-meteo', '/wp-content/plugins/gadlab-meteo/styles/gadlab-meteo.css' );
+  wp_enqueue_style( 'gadlab-meteo' );
+}
+add_action( 'wp_print_styles', 'add_glm_css' );
+
+/* Create shortcode
+----------------------------------------------------------------------------- */
+function glm_decode( $atts ) {
+  // Get the json data file to be parsed
+  $data_json = file_get_contents( 'https://prevision-meteo.ch/services/json/le-marchairuz' );
+  // Liste des paramètres retournés par le flux JSON:
+  // https://www.prevision-meteo.ch/uploads/pdf/recuperation-donnees-meteo.pdf
+  // Parse file to get Data
+  $decoded = json_decode( $data_json );
+
+  // Build the meteo elements in get_html_translation_table
+  $glm_icon = '<img src="'.$decoded->current_condition->icon.'" alt="icon">';
+  $glm_temp = $decoded->current_condition->tmp."°C";
+  $glm_cond = $decoded->current_condition->condition;
+  $glm_wind = _e( 'Wind', 'glm' ).' : <b>'.$decoded->current_condition->wnd_dir.'</b> '._e( 'at', 'glm' ).' <b>'.$decoded->current_condition->wnd_spd.' '._e( 'km/h', 'glm' ).'</b>';
+  $glm_sun  = '<i class="fas fa-sun" style="color:#fee034;"></i> <i class="fas fa-arrow-up"></i> '.$decoded->city_info->sunrise.' <i class="fas fa-arrow-down"></i> '.$decoded->city_info->sunset;
+  $glm_humid = '<i class="fas fa-tint" style="color:#00c3ff;"></i>&nbsp; '._e( 'Humidity', 'glm' ).' : <b>'.$decoded->current_condition->humidity.'%</b>';
+
+  // Build the HTML widget
+  $content  = '<a href="/informations/meteo/" title="'._e( 'Consult the detailed forecasts...', 'glm' ).'">';
+  $content .= '<div id="meteo_today">';
+  $content .= '<p class="meteo-temperature">'.$glm_temp.'&nbsp;'.$glm_icon.'</p>';
+  $content .= '<p class="meteo-conditions">'.$glm_cond.'<br/>';
+  $content .= $glm_wind.'<br/>';
+  $content .= $glm_sun.'<br/>';
+  $content .= $glm_humid.'</p>';
+  $content .= '</div></a><!-- /#widget_meteo -->';
+
+  return $content;
+}
+add_shortcode( 'meteo', 'glm_decode' );
